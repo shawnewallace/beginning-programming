c7c24eebadeff56c9d43a15048ef6a99c1337008
Shawn Wallace <shawn@wshawn.com>
2013-05-08 18:28:57 -0400

    after class #3

---
+++ big_bubble_sort.rb
@@ -0,0 +1,23 @@
+toSort = Array.new
+r = Random.new
+
+(0..1000).each do 
+  toSort << r.rand(0..100000)
+end
+
+puts toSort
+
+response = gets
+
+swapped = true
+while swapped
+  swapped = false
+  0.upto(toSort.size - 2) do |i|
+    if toSort[i] > toSort[i+1]
+      toSort[i], toSort[i+1] = toSort[i+1], toSort[i] # swap values
+      swapped = true
+    end
+  end
+end
+
+puts toSort
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,30 +1,30 @@
-toSort = [1, 5, 3, 2, 4]
+# toSort = [1, 5, 3, 2, 4]
+# #puts toSort
+
+# for i in 0...toSort.length
+#   for j in 0...(toSort.length)
+#     if (toSort[i] <= toSort[j]) 
+#       temp = toSort[i]
+#       toSort[i] = toSort[j]
+#       toSort[j] = temp
+#     end
+#   end
+# end
+
 #puts toSort
 
-for i in 0...toSort.length
-  for j in 0...(toSort.length)
-    if (toSort[i] <=   toSort[j]) 
-      temp = toSort[i]
-      toSort[i] = toSort[j]
-      toSort[j] = temp
+toSort = [1, 5, 3, 2, 4]
+
+#another approach
+swapped = true
+while swapped
+  swapped = false
+  0.upto(toSort.size - 2) do |i|
+    if toSort[i] > toSort[i+1]
+      toSort[i], toSort[i+1] = toSort[i+1], toSort[i] # swap values
+      swapped = true
     end
   end
 end
 
 puts toSort
-
-toSort = [1, 5, 3, 2, 4]
-
-#another approach
-# swapped = true
-# while swapped
-#   swapped = false
-#   0.upto(toSort.size - 2) do |i|
-#     if toSort[i] > toSort[i+1]
-#       toSort[i], toSort[i+1] = toSort[i+1], toSort[i] # swap values
-#       swapped = true
-#     end
-#   end
-# end
-
-# puts toSort
