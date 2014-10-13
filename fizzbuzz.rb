458107b69f4d19b447c233c5a6b5fc64889aa704
Shawn Wallace <shawn@wshawn.com>
2014-10-13 18:06:32 -0400

    Updated FizzBuzz for program

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,12 @@
-class FizzBuzzCalculator
-  def getFizzBuzzNumber(i)
-    return "FizzBuzz" if (i%15 == 0)
-    return "Fizz" if (i%3 == 0)
-    return "Buzz" if (i%5 == 0)
-
-    return i
-  end
-end
-
-calculator = FizzBuzzCalculator.new
-
 (1..100).each { |i|
   # put logic here
-  puts calculator.getFizzBuzzNumber(i)
+  if (i % 3 == 0) && (i % 5 == 0)
+    puts "FizzBuzz"
+  elsif (i % 3 == 0)
+    puts "Fizz"
+  elsif (i % 5 == 0)
+    puts "Buzz"
+  else
+    puts i
+  end
 }
